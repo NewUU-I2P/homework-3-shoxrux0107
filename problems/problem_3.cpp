729d88a3c1ee2ed1c6dce3e737d5505b8ca0bd91
shoxrux0107 <s.shuxratov@newuu.uz>
2023-11-13 19:21:29 +0500

    up

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -3,22 +3,36 @@
 std::string problemSolution3(char S, float height) {
     // write your code here
 
-    if (S == 'M'){
-        if (height < 1.70){
-            return "Short";
-        } else if (height >= 1.70 || height < 1.85){
-            return "Normal";
-        } else return "Tall";
-    } else if (S == 'F'){
-        if (height < 1.60){
-            return "Short";
-        } else if (height >= 1.60 || height < 1.75){
-            return "Normal";
-        } else return "Tall";
-    } else {
-        return "Not Human";
-    }
+    if (S == 'M' && height < 1.70)
+        return "Short";
+    else if (S == 'M' && (height >= 1.70 || height < 1.85))
+        return "Normal";
+    else if (S == 'M' && (height >= 1.85))
+        return "Tall";
+    else if (S == 'F' && height < 1.60)
+        return "Short";
+    else if (S == 'F' && (height >= 1.60 || height < 1.75))
+        return "Normal";
+    else if (S == 'F' && (height >= 1.75))
+        return "Tall";
+    else return "Not Recognized";
+
 
     // use return to return your result
     // make use of control flow statements
 }
+//if (S == 'M'){
+//if (height < 1.70){
+//return "Short";
+//} else if (height >= 1.70 || height < 1.85){
+//return "Normal";
+//} else return "Tall";
+//} else if (S == 'F'){
+//if (height < 1.60){
+//return "Short";
+//} else if (height >= 1.60 || height < 1.75){
+//return "Normal";
+//} else return "Tall";
+//} else {
+//return "Not Human";
+//}
