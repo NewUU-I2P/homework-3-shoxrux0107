7afd4f84b2913f0b8414b5eaf79696c91801de1f
shoxrux0107 <s.shuxratov@newuu.uz>
2023-11-14 18:51:59 +0500

    up

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -4,21 +4,22 @@ std::string problemSolution3(char S, float height) {
     // write your code here
     std::string result;
 
-    if (S == 'M' && height < 1.70)
+    if (S == 'M' && height < 1.70) {
         result = "Short";
-    else if (S == 'M' && (height >= 1.70 || height < 1.85))
+    } else if (S == 'M' && (height >= 1.70 || height < 1.85)) {
         result = "Normal";
-    else if (S == 'M' && height >= 1.85)
+    } else if (S == 'M' && height >= 1.85) {
         result = "Tall";
-    else if (S == 'F' && height < 1.60)
+    } else if (S == 'F' && height < 1.60) {
         result = "Short";
-    else if (S == 'F' && (height >= 1.60 || height < 1.75))
+    } else if (S == 'F' && (height >= 1.60 || height < 1.75)) {
         result = "Normal";
-    else if (S == 'F' && height >= 1.75)
+    } else if (S == 'F' && height >= 1.75)
         result = "Tall";
     else result = "Not Recognized";
 
     return result;
+
     // use return to return your result
     // make use of control flow statements
 }
